9d7d75564848ad46bd177e24653dc03c44a26cf7
NhatHuyTran <nhathuy13598@gmail.com>
2020-12-31 18:09:11 +0700

    * Fix Books

--- Day 2/Books/Source.cpp
+++ Day 2/Books/Source.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 int main()
 {
 	int N, T;
-	std::vector<int> books;
 	std::cin >> N >> T;
+	std::vector<int> books(N);
 	
-	books.reserve(N);
 	for (int i = 0; i < N; i++)
 	{
-		int tmp;
-		std::cin >> tmp;
-		books.push_back(tmp);
+		std::cin >> books[i];
 	}
-
-	std::vector<int> accumulate;
-	accumulate.resize(N + 1, 0);
-	for (int i = 0; i < N; i++)
+	int i = 0;
+	int max = 0;
+	for (int j = 0; j < N; j++)
 	{
-		accumulate[i + 1] = accumulate[i] + books[i];
+		T -= books[j];
+		while (T < 0)
+		{
+			T += books[i];
+			i += 1;
+		}
+		max = std::max(max, j - i + 1);
 	}
-
-	int L = 0, R = 1;
-	int max_book = 0;
-	while (R != L)
-	{
-		if (R == N + 1)
-		{
-			break;
-		}
-		if (accumulate[R] - accumulate[L] <= T)
-		{
-			if (R - L >= max_book)
-			{
-				max_book = R - L;
-			}
-			R += 1;
-		}
-		else
-		{
-			L += 1;
-		}
-	}
-	std::cout << max_book << std::endl;
+	std::cout << max << std::endl;
+	return 0;
 }
