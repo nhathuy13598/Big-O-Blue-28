0d497ab757329d54f37a3e803dce855a78013545
NhatHuyTran <nhathuy13598@gmail.com>
2020-12-31 16:10:40 +0700

    * Fix Bear and Game

--- Day 1/Bear and Game/Source.cpp
+++ Day 1/Bear and Game/Source.cpp
@@ -4,36 +4,33 @@ int main()
 {
 	int N;
 	std::cin >> N;
-	std::vector<int> minutes;
+	std::vector<int> minutes(N);
 	for (int i = 0; i < N; i++)
 	{
-		int input;
-		std::cin >> input;
-		minutes.push_back(input);
+		std::cin >> minutes[i];
 	}
-	int watch = 0;
-	int init = 0;
+	
+	int T = 0;
 	for (int i = 0; i < N; i++)
 	{
-		if (minutes[i] - init <= 15)
+		if (minutes[i] <= T + 15)
 		{
-			watch += minutes[i] - init;
-			init = minutes[i];
+			T = minutes[i];
 		}
 		else
 		{
-			watch += 15;
-			std::cout << watch << std::endl;
-			return 0;
+			T += 15;
+			break;
 		}
 	}
-	if (watch + 15 >= 90)
+	
+	if (T > 90)
 	{
 		std::cout << 90 << std::endl;
 	}
 	else
 	{
-		std::cout << watch + 15 << std::endl;
+		std::cout << T << std::endl;
 	}
 	return 0;
 }
